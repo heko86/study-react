f730165099f70cfbf7c3e11da2dad3ba9e26c8cd
heko86 <ryou86pg@gmail.com>
2023-06-09 02:27:16 +0900

    ♻️ メイン部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -2,27 +2,12 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 
 export function Headline(props) {
+  console.log(props);
   return (
     <>
       <div className={styles.description}>
         <p>{props.page} Page</p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <div>{props.children}</div>
       </div>
       <div className={styles.center}>
         <Image
