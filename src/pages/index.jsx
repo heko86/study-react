649a56b3729273f749637c7e1b2b867671acba96
heko86 <ryou86pg@gmail.com>
2023-06-16 00:22:02 +0900

    ⚡️ useCallback,useEffectについて

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 
 export default function Home() {
   const [count, setcount] = useState(1);
-  const handleClick = (e) => {
-    //前の状態を受け取って処理する
-    setcount((count) => count + 1);
-    setcount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    (e) => {
+      //前の状態を受け取って処理する
+      console.log(count);
+      if (count < 10) {
+        setcount((count) => count + 1);
+      }
+    },
+    [count]
+  );
   useEffect(() => {
+    console.log(`マウント時${count}`);
     //基本的にNextjsの管轄内にあるDOMに直接アクセスするのは厳禁
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
+      console.log(`アンマウント時${count}`);
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
   return (
     <>
       <Header />
