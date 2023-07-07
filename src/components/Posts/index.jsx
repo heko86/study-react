d256a0a65ef8409380ddce0ca587aeb1abbf833e
heko86 <ryou86pg@gmail.com>
2023-07-07 23:32:12 +0900

    🚸 APIを叩く際のローディング状態・エラー状態を追加

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useState } from "react";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const json = await res.json();
+      if (!res.ok) {
+        throw new Error("エラーが発生したため、データの取得に失敗しました");
+      }
+      setPosts(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  if (loading) {
+    return <div>ローディング中です</div>;
+  }
+  if (error) {
+    <div>{error.message}</div>;
+  }
+  if (posts.length < 0) {
+    return <div>データは空です</div>;
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div>ローディング中です</div>
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : (
+        <div>データは空です</div>
+      )}
+    </>
+  );
+};
+export default Posts;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,12 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-
-import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import Posts from "src/components/Posts";
 
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-  console.log(posts);
+const Home = () => {
   return (
     <>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
       <Main page="Index" />
     </>
   );
