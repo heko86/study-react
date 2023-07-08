d30d158b64220699684bcdd7dc232aabcc01c180
heko86 <ryou86pg@gmail.com>
2023-07-09 03:34:31 +0900

    ⚡️ useReducer追加

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,47 +1,73 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+// stateとactionを受け取って新しいstateを返す
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    default: {
+      throw new Error("no such action type!");
+    }
+  }
+};
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
+  // stateにはinitialStateが入ってくる
+  // reducerは現在のstateとactionを受け取り新しいstateを返すもの
+  // 新しいactionを起こすのをdispatchが担う
+  // dispatchを使って新しいactionを発生させて既存のstateと組み合わせて新しいstateを生み出す
+  const [state, dispatch] = useReducer(reducer, initialState);
   const getPosts = useCallback(async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const json = await res.json();
       if (!res.ok) {
         throw new Error("エラーが発生したため、データの取得に失敗しました");
       }
-      setPosts(json);
+      const json = await res.json();
+      // dispatchで送り度溶けるtypeとdataはactionの中に入る
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "error", error });
     }
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  if (state.loading) {
     return <div>ローディング中です</div>;
   }
-  if (error) {
-    <div>{error.message}</div>;
+  if (state.error) {
+    <div>{state.error.message}</div>;
   }
-  if (posts.length < 0) {
+  if (state.data.length < 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <>
-      {loading ? (
+      {state.loading ? (
         <div>ローディング中です</div>
-      ) : error ? (
-        <div>{error.message}</div>
-      ) : posts.length > 0 ? (
+      ) : state.error ? (
+        <div>{state.error.message}</div>
+      ) : state.data.length > 0 ? (
         <ol>
-          {posts.map((post) => {
+          {state.data.map((post) => {
             return <li key={post.id}>{post.title}</li>;
           })}
         </ol>
