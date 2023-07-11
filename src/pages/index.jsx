a5b62ae4a084d54312b9a7941767328d28cc08c5
heko86 <ryou86pg@gmail.com>
2023-07-12 05:42:22 +0900

    💄 cssを更新

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
 import { Header } from "src/components/Header";
-
+import styles from "src/styles/Home.module.css";
 const Index = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       <h1>Next.jsで学ぶReact講座</h1>
       <p>JSONPlaceholderのAPIを叩く</p>
-    </>
+    </div>
   );
 };
 export default Index;
