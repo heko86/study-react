73e4260de314506f31331e9ec866a673e1a888f3
heko86 <ryou86pg@gmail.com>
2023-07-12 05:27:20 +0900

    🐛 パスの変更し忘れ修正

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import classes from "./Header.module.css";
 import Link from "next/link";
 
 const NAV_ITEMS = [
+  { href: "/", label: "Index" },
   { href: "/posts", label: "Posts" },
-  { href: "/about", label: "About" },
 ];
 
 export const Header = (props) => {
