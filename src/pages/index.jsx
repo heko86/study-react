24300f6a57dc9444876e1002cf8bbc22560b9ac6
heko86 <ryou86pg@gmail.com>
2023-07-12 05:27:41 +0900

    🔥 不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,15 @@
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useBgLightBlue } from "src/hooks/useBgColor";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const About = ({
-  doubleCount,
-  isShow,
-  handleClick,
-  handlDisplay,
-  text,
-  array,
-  handleChange,
-  handleAdd,
-}) => {
+const Index = () => {
   return (
     <>
       <Header />
-      {isShow ? <h1>{doubleCount}</h1> : null}
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handlDisplay}>{isShow ? "非表示" : "表示"}</button>
-
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-      <Main page="About" />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIを叩く</p>
+      <Main page="Index" />
     </>
   );
 };
-export default About;
+export default Index;
